6965fcfca78620cc1fc782c040575ec758204706
A2L5E0X1 <alexed25@protonmail.com>
2022-04-29 20:53:20 +0200

    extra: move rom additions to vendor_extra
    * to allow other's to see my rom additions without leaking my private keys
    
    Commit history of vendor/a2l5:
    
    commit 02a0b4b26efb2ae7105b0a9a928eb7f01278802e (HEAD -> master)
    Author: A2L5E0X1 <alexed25@protonmail.com>
    Date:   Fri Apr 29 20:48:48 2022 +0200
    
        a2l5: move rom additions to vendor_extra
    
    commit a5961a7d204cdb3604e2498cb3205c6241ef8025 (origin/master, origin/HEAD)
    Author: A2L5E0X1 <alexed25@protonmail.com>
    Date:   Mon Dec 27 13:46:00 2021 +0100
    
        a2l5: disable jack when building oreo
    
    commit 0a460a5bf61d71e54316b55a49cfb9c2153a31b0
    Author: A2L5E0X1 <alexed25@protonmail.com>
    Date:   Fri Sep 3 20:27:34 2021 +0200
    
        a2l5: update vendor repo
    
    commit d126055b90c3c891d381d47588565a3510652dd1
    Author: A2L5E0X1 <alexed25@protonmail.com>
    Date:   Mon Jul 12 16:38:13 2021 +0200
    
        a2l5: add keys for cyngn-priv-app
    
        * Needed for LineageOS 14.1
        ninja: error: 'vendor/a2l5/keys/cyngn-priv-app.pk8', needed by '/home/alex2501/build/cm-14.1/out/target/product/ac50fnev2/obj/APPS/CMAudioService_intermediates/package.apk', missing and no known rule to make it
    
    commit b2bcd371433e271a9e3859bcf946cb06c70bb3dc
    Author: Alex <alexed25@protonmail.com>
    Date:   Sat Jun 26 06:42:09 2021 -0400
    
        a2l5: initial commit

--- product.mk
+++ product.mk
@@ -1,2 +1,41 @@
-include vendor/a2l5/priv-vendor.mk
+#
+# Copyright (C) 2021-2022 A2L5E0X1
+#
 
+#
+# This repository includes private stuff like dev-keys used to sign the builds.
+# Do not share content of this repository without permissions!
+#
+
+PRIV_VENDOR_PATH := vendor/extra
+
+#
+# Sign keys
+#
+
+## MOVED TO VENDOR_A2L5
+
+#ifneq ($(SIGN_WITH_TEST_KEYS),true)
+#    $(warning Using a2l5 priv-keys for signing.)
+#    PRODUCT_DEFAULT_DEV_CERTIFICATE := vendor/a2l5/keys/releasekey
+#endif
+
+-include vendor/a2l5/priv-keys.mk
+
+#
+# OTA
+#
+
+ifneq ($(filter sample,$(TARGET_DEVICE)),)
+    $(warning Device has OTA support!)
+    DEVICE_PACKAGE_OVERLAYS += $(PRIV_VENDOR_PATH)/ota
+endif
+
+#
+# Disable Jack when building Oreo
+#
+
+ifeq ($(PLATFORM_VERSION),8.1.0)
+    $(warning Oreo detected! Jack will be disabled...)
+    ANDROID_COMPILE_WITH_JACK := false
+endif
